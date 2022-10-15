601a524c368f822f6b39ace097f471b8c0d30129
Marcelo-Drummond-Fonseca <48736981+Marcelo-Drummond-Fonseca@users.noreply.github.com>
2022-10-15 03:26:41 -0300

    Update spheres.js

--- spheres.js
+++ spheres.js
@@ -1,7 +1,6 @@
 document.body.onload = function(){
 	var buttons = document.createElement("div");
 	buttons.className = "container";
-	
 	var but1 = document.createElement("button");
 	but1.innerHTML = "+1 bola";
 	but1.style
@@ -9,7 +8,6 @@ document.body.onload = function(){
 		newCircle();
 	}
 	buttons.appendChild(but1);
-	
 	var but5 = document.createElement("button");
 	but5.innerHTML = "+5 bolas";
 	but5.onclick = function(){
@@ -20,7 +18,6 @@ document.body.onload = function(){
 		newCircle();
 	}
 	buttons.appendChild(but5);
-	
 	var but10 = document.createElement("button");
 	but10.innerHTML = "+10 bolas";
 	but10.onclick = function(){
@@ -36,12 +33,9 @@ document.body.onload = function(){
 		newCircle();
 	}
 	buttons.appendChild(but10);
-	
 	document.body.appendChild(buttons);
-	
 	var line = document.createElement("hr");
 	document.body.appendChild(line);
-	
 	newCircle();
 	newCircle();
 	newCircle();
